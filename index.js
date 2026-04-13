a9bc8870e9a48bf282cf8112ab77f39d2effc7a9
yazzyfresh <yazzy06ramirez@aol.com>
2026-04-13 12:43:29 -0400

    node index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const fs = require( 'fs' );
+
+// Blocking, synchronous way
+// const textIn = fs.readFileSync('./txt/input. txt', 'utf-8');
+// console. log(textIn) ;
+// const textOut = 'This is what we know about the avocado: ${textIn} . \nCreated on ${Date.now()}
+
+// fs.writeFileSync('./txt/output.txt", textOut);
+// console. log( 'File written! ') ;
+
+// Non-blocking, asynchronous way
+fs. readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  fs. readFile('./txt/start.txt', 'utf-8', (err, data2) => {
+console. log(data2) ;
+  });
+}) ;
+console. log( 'Will read file!' ) ;
+
+
