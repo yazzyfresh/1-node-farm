235726a17d86efbfebaf15cdefb4b130442aff2f
yazzyfresh <yazzy06ramirez@aol.com>
2026-04-16 13:42:47 -0400

    attemping to find errors in code

--- index.js
+++ index.js
@@ -31,9 +31,10 @@ const http = require("http");
 /////////////////////////////
 // Server
 const server = http.createServer((req, res) => {
+  console.log(req);
   res.end("Hello from the server!");
 });
 
 server.listen(8000 , '127.0.0.1' () => {
   console.log("Listening to requests on port 8000");
-});
+})
