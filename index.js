c81b5eed8c813b5dd001fc010884787d0dda778d
yazzyfresh <yazzy06ramirez@aol.com>
2026-04-16 12:41:38 -0400

    prior to lunch

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const fs = require("fs");
+const http = require("http");
+
+/////////////////////////////
+// FILES
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input. txt', 'utf-8');
 // console. log(textIn) ;
-// const textOut = 'This is what we know about the avocado: ${textIn} . \nCreated on ${Date.now()}
-
+// const textOut = 'This is what we know about the avocado: ${textIn} . \nCreated on ${Date.now()}. ' ;
 // fs.writeFileSync('./txt/output.txt", textOut);
 // console. log( 'File written! ') ;
 
 // Non-blocking, asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("Error!");
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("Error!");
 
-  fs.readFile("./txt/${data1}.txt", "utf-8", (err, data2) => {
-    console.log(data2);
+//   fs.readFile("./txt/${data1}.txt", "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
 
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
+//       fs.writeFile("./txt/final.txt", "${data2}\n${data3}", "utf-8", (err) => {
+//         console.log("Your file has been written!");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file!");
 
-      fs.writeFile("./txt/final.txt", "${data2}\n${data3}", "utf-8", (err) => {
-        console.log("Your file has been written!");
-      });
-    });
-  });
+/////////////////////////////
+// Server
+const server = http.createServer((req, res) => {
+  res.end("Hello from the server!");
 });
-console.log("Will read file!");
+
+server.listen(8000 , '127.0.0.1' () => {
+  console.log("Listening to requests on port 8000");
+});
