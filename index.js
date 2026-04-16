f8e86b25d779f583ac9632874afead0cf59ee390
yazzyfresh <yazzy06ramirez@aol.com>
2026-04-16 09:31:11 -0400

    prior work

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs = require( 'fs' );
+const fs = require("fs");
 
 // Blocking, synchronous way
 // const textIn = fs.readFileSync('./txt/input. txt', 'utf-8');
@@ -9,23 +9,19 @@ const fs = require( 'fs' );
 // console. log( 'File written! ') ;
 
 // Non-blocking, asynchronous way
-fs. readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-    if(err) return console. log('Error!') ;
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("Error!");
 
-  fs. readFile('./txt/${data1}.txt', 'utf-8', (err, data2) => {
-    console. log(data2) ;
+  fs.readFile("./txt/${data1}.txt", "utf-8", (err, data2) => {
+    console.log(data2);
 
-    fs. readFile('./txt/append.txt', 'utf-8', (err, data3) => {
-      console. log(data3) ;
+    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+      console.log(data3);
 
-      fs. writeFile('./txt/final.txt', '${data2}\n${data3}', 'utf-8', err => {
-        console. log('Your file has been written!') ;
-      })
-      }) ;
+      fs.writeFile("./txt/final.txt", "${data2}\n${data3}", "utf-8", (err) => {
+        console.log("Your file has been written!");
+      });
+    });
   });
-}) ;
-console. log( 'Will read file!' ) ;
-
-
-
-
+});
+console.log("Will read file!");
