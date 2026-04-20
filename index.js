fac5e2da1a893a26143b7a1a50c2dc6f4881aa78
yazzyfresh <yazzy06ramirez@aol.com>
2026-04-20 09:37:39 -0400

    fav icon url

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const http = require("http");
+const url = require("url");
 
 /////////////////////////////
 // FILES
@@ -31,6 +32,7 @@ const http = require("http");
 /////////////////////////////
 // Server
 const server = http.createServer((req, res) => {
+  console.log(req.url);
   res.end("Hello from the server!");
 });
 
