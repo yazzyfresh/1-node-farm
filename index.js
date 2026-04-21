6d483dd2e3cde1a71ffc1a4aead4c2353b1192d8
yazzyfresh <yazzy06ramirez@aol.com>
2026-04-21 13:33:31 -0400

    reloading server

--- index.js
+++ index.js
@@ -34,27 +34,28 @@ const url = require("url");
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
-if(pathName === '/' || pathName ==='/overview'){
-  res.end('This is the OVERVIEW');
-} else if(pathName ==='/product'){
-  res.end('This is the PRODUCT');
-}else if(pathName ==='/api'){ 
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    fs.readFile("${__dirname}/dev-data/data.json", "utf-8", (err, data) => {
+      const productData = JSON.parse(data);
+      console.log(productData);
+      res.writeHead(200, {'Content-type':'application/json'});
+      res.end(data);
+    });
 
-  fs.readFile('${__dirname}/dev-data/data.json', 'utf-8', (err, data) => {
-    const productData = JSON.parse(data);
-    console.log(productData);
-  });
-
-  res.end('API');
-}else {
-  res.writeHead(404, {
-    'content-type': 'text/html',
-    'my-own-header': 'hello-world'
-  });
-  res.end('<h1>Page not found!</h1>');
-}
+    
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
 });
 
-server.listen(8000 , '127.0.0.1', () => {
+server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to requests on port 8000");
 });
