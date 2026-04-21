5c2cf2c2bb513ad9e69b381c3e129bdb2e41c267
yazzyfresh <yazzy06ramirez@aol.com>
2026-04-21 13:45:36 -0400

    attepting to find error???

--- index.js
+++ index.js
@@ -34,17 +34,18 @@ const url = require("url");
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
-  if (pathName === "/" || pathName === "/overview") {
+  if (pathName === '/' || pathName === '/overview') {
     res.end("This is the OVERVIEW");
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
   } else if (pathName === "/api") {
+
     fs.readFile("${__dirname}/dev-data/data.json", "utf-8", (err, data) => {
       const productData = JSON.parse(data);
       res.writeHead(200, { "Content-type": "application/json" });
       res.end(data);
-    })
-    
+    });
+
   } else {
     res.writeHead(404, {
       "content-type": "text/html",
