7fce20e3556bb5b357105c5f5ec06cf9cc09f04d
yazzyfresh <yazzy06ramirez@aol.com>
2026-04-21 13:36:44 -0400

    sintax

--- index.js
+++ index.js
@@ -42,11 +42,9 @@ const server = http.createServer((req, res) => {
     fs.readFile("${__dirname}/dev-data/data.json", "utf-8", (err, data) => {
       const productData = JSON.parse(data);
       console.log(productData);
-      res.writeHead(200, {'Content-type':'application/json'});
+      res.writeHead(200, { "Content-type": "application/json" });
       res.end(data);
     });
-
-    
   } else {
     res.writeHead(404, {
       "content-type": "text/html",
