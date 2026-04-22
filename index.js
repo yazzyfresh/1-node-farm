792099ddc41f87e50c9ceb34b3ff8d5a15bf9e1e
yazzyfresh <yazzy06ramirez@aol.com>
2026-04-22 12:52:14 -0400

    edited from video

--- index.js
+++ index.js
@@ -46,7 +46,11 @@ const server = http.createServer((req, res) => {
     res.end("This is the PRODUCT");
   } else if (pathName === "/api") {
 
-    
+    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+      const productData = JSON.parse(data);
+      res.writeHead(200, { "Content-type": "application/json" });
+      res.end(data);
+    });
 
   } else {
     res.writeHead(404, {
