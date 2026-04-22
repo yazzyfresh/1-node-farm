e0c8f94f83ebb455cabf2946be0ee4a9c2129efd
yazzyfresh <yazzy06ramirez@aol.com>
2026-04-22 10:08:32 -0400

    found error

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const server = http.createServer((req, res) => {
     res.end("This is the PRODUCT");
   } else if (pathName === "/api") {
 
-    fs.readFile("${__dirname}/dev-data/data.json", "utf-8", (err, data) => {
+    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
       const productData = JSON.parse(data);
       res.writeHead(200, { "Content-type": "application/json" });
       res.end(data);
