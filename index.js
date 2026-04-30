a3357a0fcba14aed592a03f0e6cf82ab3917ce4d
yazzyfresh <yazzy06ramirez@aol.com>
2026-04-29 21:12:31 -0400

    ???

--- index.js
+++ index.js
@@ -32,16 +32,16 @@ const url = require("url");
 /////////////////////////////
 // Server
 const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
   output = output.replace(/{%IMAGE%}/g, product.image);
   output = output.replace(/{%PRICE%}/g, product.price);
   output = output.replace(/{%FROM%}/g, product.from);
   output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUALITY%}/g, product.quantity);
+  output = output.replace(/{%QUALITY%}/g, product.quantity);
   output = output.replace(/{%DISCRIPTION%}/g, product.discription);
   output = output.replace(/{%ID%}/g, product.id);
 
-  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
   return output;
 }
 
