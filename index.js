6ce18dd1c1348eb505f0ad566166fd82212f8eab
yazzyfresh <yazzy06ramirez@aol.com>
2026-05-04 10:46:24 -0400

    issues???

--- index.js
+++ index.js
@@ -3,33 +3,6 @@ const http = require("http");
 const url = require("url");
 
 /////////////////////////////
-// FILES
-
-// Blocking, synchronous way
-// const textIn = fs.readFileSync('./txt/input. txt', 'utf-8');
-// console. log(textIn) ;
-// const textOut = 'This is what we know about the avocado: ${textIn} . \nCreated on ${Date.now()}. ' ;
-// fs.writeFileSync('./txt/output.txt", textOut);
-// console. log( 'File written! ') ;
-
-// Non-blocking, asynchronous way
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//   if (err) return console.log("Error!");
-
-//   fs.readFile("./txt/${data1}.txt", "utf-8", (err, data2) => {
-//     console.log(data2);
-//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-//       console.log(data3);
-
-//       fs.writeFile("./txt/final.txt", "${data2}\n${data3}", "utf-8", (err) => {
-//         console.log("Your file has been written!");
-//       });
-//     });
-//   });
-// });
-// console.log("Will read file!");
-
-/////////////////////////////
 // TEMPLATE REPLACER
 const replaceTemplate = (temp, product) => {
   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
@@ -41,8 +14,9 @@ const replaceTemplate = (temp, product) => {
   output = output.replace(/{%DESCRIPTION%}/g, product.description);
   output = output.replace(/{%ID%}/g, product.id);
 
-  if (!product.organic)
+  if (!product.organic) {
     output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  }
 
   return output;
 };
@@ -51,18 +25,24 @@ const replaceTemplate = (temp, product) => {
 // READ FILES
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
-  "utf-8",
-);
-const tempCard = fs.readFileSync(
-  `${__dirname}/templates/template-card.html`,
-  "utf-8",
-);
-const tempProduct = fs.readFileSync(
-  `${__dirname}/templates/template-product.html`,
-  "utf-8",
+  "utf-8"
 );
 
-const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(
+  `${__dirname}/dev-data/data.json`,
+  "utf-8"
+);
+
 const dataObj = JSON.parse(data);
 
 /////////////////////////////
@@ -70,40 +50,52 @@ const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
   const { pathname, query } = url.parse(req.url, true);
 
-  // Overview page
+  // OVERVIEW PAGE
   if (pathname === "/" || pathname === "/overview") {
     res.writeHead(200, { "Content-type": "text/html" });
 
-    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join("");
+    const cardsHtml = dataObj
+      .map(el => replaceTemplate(tempCard, el))
+      .join("");
 
-    console.log(cardsHtml);
-
-    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    const output = tempOverview.replace(
+      /{%PRODUCT_CARDS%}/g,
+      cardsHtml
+    );
 
     res.end(output);
+  }
 
-    // Product page
-  } else if (pathname === "/product") {
+  // PRODUCT PAGE
+  else if (pathname === "/product") {
     const product = dataObj[query.id];
+
+    if (!product) {
+      res.writeHead(404, { "Content-type": "text/html" });
+      return res.end("<h1>Product not found</h1>");
+    }
+
     const output = replaceTemplate(tempProduct, product);
+
     res.writeHead(200, { "Content-type": "text/html" });
     res.end(output);
+  }
 
-    // API
-  } else if (pathname === "/api") {
+  // API
+  else if (pathname === "/api") {
     res.writeHead(200, { "Content-type": "application/json" });
     res.end(data);
+  }
 
-    // Not found
-  } else {
+  // NOT FOUND
+  else {
     res.writeHead(404, {
       "Content-type": "text/html",
-      "my-own-header": "hello-world",
     });
     res.end("<h1>Page not found!</h1>");
   }
 });
 
 server.listen(8000, "127.0.0.1", () => {
-  console.log("Listening to requests on port 8000");
+  console.log("Server running on http://127.0.0.1:8000");
 });
