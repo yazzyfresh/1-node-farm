86f8c297379df165c1309dea35b7241f81bb0850
yazzyfresh <yazzy06ramirez@aol.com>
2026-05-07 13:38:00 -0400

    time stamp 1:44

--- index.js
+++ index.js
@@ -3,31 +3,8 @@ const http = require('http');
 const url = require('url');
 
 /////////////////////////////
-// TEMPLATE REPLACER
+// Server
 
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
-
-  // ORGANIC BADGE
-  if (!product.organic) {
-    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  } else {
-    output = output.replace(/{%NOT_ORGANIC%}/g, '');
-  }
-
-  return output;
-};
-
-/////////////////////////////
-// READ FILES
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
@@ -50,9 +27,6 @@ const data = fs.readFileSync(
 );
 
 const dataObj = JSON.parse(data);
-
-/////////////////////////////
-// CREATE SERVER
 
 const server = http.createServer((req, res) => {
   const { pathname, query } = url.parse(req.url, true);
